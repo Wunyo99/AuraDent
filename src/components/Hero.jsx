456d8a5cd1b017c8f4def88d207d0d2130179faa
Wunyo99 <mawunyoanani4@gmail.com>
2026-07-23 03:15:32 +0000

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,54 @@
+import { MoveRight, Search } from "lucide-react";
+import { motion } from "framer-motion";
+const Hero = () => {
+  return (
+    <>
+      <section
+        id="home"
+        className="h-screen flex items-center justify-center md:justify-start overflow-hidden"
+      >
+        <div
+          className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+          style={{
+            backgroundImage:
+              "url('https://t3.ftcdn.net/jpg/06/48/55/72/240_F_648557265_JIjNtL5RdUZ8AHdgBh7PkIMQqdinaOgB.jpg')",
+          }}
+        />
+        <div
+          className="absolute inset-0"
+          style={{
+            background:
+              "linear-gradient(to right, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0.1) 35%, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0) 100%)",
+          }}
+        />
+        <motion.div
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          className="relative max-w-2xl px-6 md:px-15 text-center md:text-start space-y-10 z-10"
+        >
+          <h1 className="text-4xl md:text-8xl text-white font-bold">
+            Seemles <span className="text-teal-700">Dental Care</span>
+          </h1>
+
+          <p className="text-white/90 font-medium text-xl">
+            Premium dental care designed around your comfort, health, and
+            confidence.
+          </p>
+
+          <div className="flex flex-col md:flex-row items-center justify-center md:justify-start gap-5">
+            <a
+              to="/jobs"
+              className="flex items-center gap-2 bg-white py-4 px-5 rounded-full font-medium hover:bg-yellow-200 duration-300"
+            >
+              <span>Book Appointment</span>
+              <MoveRight />
+            </a>
+          </div>
+        </motion.div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,191 @@
+import { useState } from "react";
+import { Menu, Phone, Stethoscope } from "lucide-react";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+import { Button } from "@/components/ui/button";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+
+const navLinks = [
+  {
+    name: "Home",
+    href: "#home",
+  },
+  {
+    name: "About",
+    href: "#about",
+  },
+  {
+    name: "Services",
+    href: "#services",
+  },
+  {
+    name: "Doctors",
+    href: "#doctors",
+  },
+  {
+    name: "Testimonials",
+    href: "#testimonials",
+  },
+  {
+    name: "Contact",
+    href: "#contact",
+  },
+];
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header
+      className="
+      sticky
+      top-0
+      z-50
+      w-fu
+      bg-blue-5
+      backdrop-blur-
+    "
+    >
+      <div className="containe mx-aut w-full flex h-20 items-center justify-between px-4">
+        <a
+          href="#home"
+          className="
+flex
+            items-center           gap-2
+          "
+        >
+          <div
+            className="
+            flex
+            h-10
+            w-10
+            items-center
+            justify-center
+            rounded-full
+            bg-teal-600
+            text-white
+          "
+          >
+            <Stethoscope size={22} />
+          </div>
+
+          <div>
+            <h1
+              className="
+              text-xl
+              font-bold
+              text-white
+            "
+            >
+              AuraDent
+            </h1>
+
+            <p
+              className="
+              text-xs
+              text-white
+            "
+            >
+              Dental Care
+            </p>
+          </div>
+        </a>
+
+        <NavigationMenu className="hidden lg:flex">
+          <NavigationMenuList className="gap-2">
+            {navLinks.map((link) => (
+              <NavigationMenuItem key={link.name}>
+                <a
+                  href={link.href}
+                  className="
+                      rounded-md
+                      px-4
+                      py-2
+                      text-sm
+                      font-medium
+                      text-white
+                      transition
+                      hover:bg-teal-50
+                      hover:text-teal-700
+                    "
+                >
+                  {link.name}
+                </a>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+
+        <div className=" hidden lg:flex">
+          <button className=" flex items-center gap-2 bg-teal-600 hover:bg-teal-700 rounded-full py-2 px-3 text-white">
+            <Phone size={18} />
+            <span>Call Now</span>
+          </button>
+        </div>
+
+        <div className="lg:hidden">
+          <Sheet open={open} onOpenChange={setOpen}>
+            <SheetTrigger asChild>
+              <Button variant="ghost" size="icon" className="md:hidden">
+                <Menu />
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent
+              side="right"
+              className="
+              w-72
+            "
+            >
+              <div
+                className="
+              mt-10
+              flex
+              flex-col
+              gap-4
+            "
+              >
+                {navLinks.map((link) => (
+                  <a
+                    key={link.name}
+                    href={link.href}
+                    onClick={() => setOpen(false)}
+                    className="
+                      rounded-lg
+                      px-4
+                      py-3
+                      text-lg
+                      font-medium
+                      text-slate-700
+                      hover:bg-teal-50
+                      hover:text-teal-700
+                    "
+                  >
+                    {link.name}
+                  </a>
+                ))}
+
+                <Button
+                  className="
+                  mt-4
+                  bg-teal-600
+                  hover:bg-teal-700
+                "
+                >
+                  Book Appointment
+                </Button>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Navbar;
