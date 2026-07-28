5fef6261a78978598a1e7ee4218349b6e23b04c3
Wunyo99 <mawunyoanani4@gmail.com>
2026-07-28 00:25:53 +0000

    UI update in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa";
 import logo2 from "../assets/logo-2.png";
 import doctors2 from "../assets/doctors2.png";
+import { motion } from "framer-motion";
 const socialLinks = [
   {
     title: "Facebook",
@@ -28,7 +29,12 @@ const Footer = () => {
     <footer className="px-5 md:px-10 py-16 bg-teal-600 rounded-t-4xl">
       <section>
         <div className="flex flex-col md:flex-row gap-5 justify-between">
-          <div>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: 0.2 }}
+          >
             <h4 className="text-5xl text-white max-w-md mb-5 font-semibold">
               Get The Smile You've Always Wanted
             </h4>
@@ -43,9 +49,14 @@ const Footer = () => {
                 </a>
               ))}
             </div>
-          </div>
+          </motion.div>
           <div className="flex flex-col md:flex-row items-cente gap-15">
-            <div>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 1, delay: 0.2 }}
+            >
               <h4 className="font-semibold text-xl text-white mb-5">Company</h4>
               <div className="flex flex-col gap-2 text-black">
                 <a href="#about" className="hover:text-white">
@@ -61,8 +72,13 @@ const Footer = () => {
                   FAQs
                 </a>
               </div>
-            </div>
-            <div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 1, delay: 0.2 }}
+            >
               <h4 className="font-semibold text-xl text-white mb-5">
                 Services
               </h4>
@@ -80,28 +96,43 @@ const Footer = () => {
                   Dental Implants
                 </a>
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
       </section>
 
       <section className="mt-10 md:mt-5 flex flex-col md:flex-row gap-5 md:items-center">
-        <div>
+        <motion.div
+          initial={{ opacity: 0, x: -20 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 1, delay: 0.2 }}
+        >
           <div className="flex gap-2 items-center">
             <div className="bg-white rounded-full">
               {" "}
               <img src={logo2} alt="" className="w-15 md:w-50" />
             </div>
-            <h5 className="text-5xl lg:text-9xl font-semibold text-teal-200 italic">AuraDent</h5>
+            <h5 className="text-5xl lg:text-9xl font-semibold text-teal-200 italic">
+              AuraDent
+            </h5>
           </div>
-        </div>
-        <div>
+        </motion.div>
+        <motion.div
+                  initial={{ opacity: 0, x:20 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 1, delay: 0.2 }}
+        >
           <img src={doctors2} alt="" className="w-70 md:w-150" />
-        </div>
+        </motion.div>
       </section>
       <hr />
       <div className="text-center">
-        <p className="py-2 text-white">© {new Date().getFullYear()} ALL RIGHT RESERVED BY <span className="font-semibold">AuraDent</span></p>
+        <p className="py-2 text-white">
+          © {new Date().getFullYear()} ALL RIGHT RESERVED BY{" "}
+          <span className="font-semibold">AuraDent</span>
+        </p>
       </div>
     </footer>
   );
